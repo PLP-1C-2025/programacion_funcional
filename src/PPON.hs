904b33f9e2fd27fc6e4dec20a17ee02fb66199a2
Thiago Krivonosoff <thiagokribas@gmail.com>
2025-04-05 18:40:41 -0300

    Ejercicio 6

--- src/PPON.hs
+++ src/PPON.hs
@@ -7,14 +7,14 @@ data PPON
   | IntPP Int
   | ObjetoPP [(String, PPON)]
   deriving (Eq, Show)
-
+{- 
 foldPPON :: (String -> b) -> (Int -> b) -> ([(String, PPON)] -> b -> b) -> PPON -> b
 foldPPON casoTexto casoInt casoObjeto pp = case pp of
   TextoPP texto -> casoTexto texto
   IntPP numero -> casoInt numero
   ObjetoPP (tupla:listaTuplas) -> (casoObjeto tupla)
  where rec = foldPPON casoTexto casoInt casoObjeto
-
+ -}
 -- longitudPPON :: PPON -> Int
 -- longitudPPON pp = foldPPON (const 1) (const 1) (\) pp 
 
@@ -24,8 +24,13 @@ pponAtomico pp = case pp of
   IntPP _ -> True 
   ObjetoPP _ -> False
 
+
+-- Interpretamos que si le pasas un pponAtomico es ademas un ObjetoSimple
 pponObjetoSimple :: PPON -> Bool
-pponObjetoSimple pp = 
+pponObjetoSimple pp = case pp of
+  TextoPP _ -> True -- Preguntar
+  IntPP _ -> True -- Preguntar 
+  ObjetoPP xs -> foldr (\(_, ppon) -> (&&) (pponAtomico ppon)) True xs
 
 intercalar :: Doc -> [Doc] -> Doc
 intercalar = error "PENDIENTE: Ejercicio 7"
