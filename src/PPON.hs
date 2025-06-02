4ade6e6375bd3bc9348c250a36193a9bf45c2367
ianfulmar <ian@ful-mar.net>
2025-06-02 17:55:46 -0300

    last

--- src/PPON.hs
+++ src/PPON.hs
@@ -45,27 +45,14 @@ aplanar :: Doc -> Doc
 aplanar = foldDoc vacio (\t1 rec -> texto t1 <+> rec) (\_ rec -> texto " " <+> rec)
 
 {-
-pponAdoc cumple con el esquema global, ya que no posee contadores ni acumuladores (esquema iterativo),
-y además es primitivo y estructural, ya que operamos sobre una lista de tipo (String, PPON) y no sobre un solo elemento, por lo que.
-
-No es un esquema iterativo porque no posee contadores ni acumuladores
-No alcanza un esquema estructural porque operamos sobre el resto del PPON en: pponADoc (snd tupla)
-No alcanza un esquema primitivo porque usamos todas las llamadas recursivas anteriores al hacer 
-map (\(elemTexto, elemPPON) -> texto (show elemTexto) <+> texto ": " <+> pponADoc elemPPON) pps
-
-
 Recordemos la definición de recursión primitiva. 
 Utiliza un caso base fijo y un caso recursivo. En el caso recursivo, puede utilizar xs, x y (g xs).
 
-Utilizamos nuestra recursión en la lambda del map sobre todos los objetos "hijos" del objeto pps. Esta recursión se aplica una sola vez sobre
+Utilizamos nuestra recursión en la lambda del map sobre todos los objetos "hijos" de pps. Esta recursión se aplica una sola vez sobre
 estos objetos. Esta recursión en sí es estructural ya que no estamos utilizando al objeto padre en sí (ObjetoPP pps), sino que utilizaríamos pps y 
 sus hijos.
 Luego, en la guarda del if, notamos que rompemos esta recursión estructural haciendola primitiva, ya que pasamos a usar (ObjetoPP pps) por afuera de 
 alguna recursión.
-
-
-El tipo de recursion es primitiva ya que la recursion en rec es estructural en pps. Pero en la guarda del if estoy manipulando 
-el pps más que para solo la recursion, por lo que estaría rompiendo la estructural, pasando a la recursion primitiva
 -} 
 
 pponADoc :: PPON -> Doc
